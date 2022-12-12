cf9d6f48e16ea3a98802220047613cc7badd1c3d
Ayesha Tabrez <ayesha.tabrez@student.manchester.ac.uk>
2022-12-12 13:19:03 +0000

    Submitted modified algortihms.py

--- algorithms.py
+++ algorithms.py
@@ -5,17 +5,47 @@ import cv2
 
 def dist_thresholding(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1 ,des2 ,k=1500)
+    l = []
+    for i in matches:
+        l1 = []
+        for j in i:
+            if j.distance < threshold_value:
+                l1.append(j)
+              
+        l.append(l1)
+    return l
 
 
 def nn(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
-
+    matches = bf.knnMatch(des1 ,des2 ,k=1)
+    l = []
+    for i in matches:
+        l1 = []
+        for j in i:
+            if j.distance < threshold_value or threshold_value==-1:
+                l1.append(j)
+            else:
+                l1.append([])
+              
+        l.append(l1[0])
+    return l
 
 def nndr(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1 ,des2 ,k=2)
+    l = []
+    for i in range(len(matches)):
+        l1 = []
+        if((matches[i][0].distance/matches[i][1].distance)<threshold_value):
+            l1.append(matches[i][0])
+        else:
+            l1.append([])
+        l.append(l1)
+
+
+    return l
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # vim:set et sw=4 ts=4:
